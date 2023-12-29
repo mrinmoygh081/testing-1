d50109073f3bfb9753ee9255528c5f4da938bdc1
Mrinmoy Ghosh <mrinmoygh081@gmail.com>
2023-12-29 22:23:11 +0530

    my 2nd commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>TPathshala is the best.</p>
+        <p>TPathshala is the best. I know from my heart.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
